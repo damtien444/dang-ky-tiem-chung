eb27f74b77b399a5f1753b92a7e19376832bb4d9
damti <damtien444@gmail.com>
2022-01-03 14:53:25 +0700

    fix confict from Tien's Local

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 from typing import overload
 
 from flask import Flask, jsonify, request, make_response
+
 from flasgger import swag_from
 import pymongo
 from bson.objectid import ObjectId
@@ -12,11 +13,24 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 
 
+# Helper
+
+
 class MyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, ObjectId):
             return str(obj)
         return super(MyEncoder, self).default(obj)
+
+
+def parse_to_date(date_json):
+    try:
+        return datetime.strptime(date_json, "%Y-%m-%d")
+    except:
+        return False
+
+
+# Init config
 
 
 app = Flask(__name__)
@@ -25,10 +39,15 @@ app.config['SECRET_KEY'] = "khoabimat"
 app.json_encoder = MyEncoder
 app.app_context()
 
+# Database Model
+
+
 client = pymongo.MongoClient(
     "mongodb+srv://vaccineForThePeople:vaccine@vaccineforthepeople.vodjz.mongodb.net/admin?retryWrites=true&w=majority")
 db = client['vaccinePlanningDB']
 managers = db['manager']
+news_collection = db['news']
+sign_collection = db['vaccination_sign']
 
 
 class User:
@@ -62,6 +81,55 @@ class User:
         return gen_dict
 
 
+class News:
+    def __init__(self, _id, title, content):
+        self._id = _id
+        self.title = title
+        self.content = content
+
+
+class Sign:
+    def __init__(self, _id, name, birthday, sex, phone, email, cccd, bhxh_id, address, priority_group, vaccine_shots):
+        self._id = _id
+        self.name = name
+        self.birthday = parse_to_date(birthday)
+        self.sex = sex
+        self.phone = phone
+        self.email = email
+        self.cccd = cccd
+        self.bhxh_id = bhxh_id
+        self.address = {'province': address['provine'],
+                        'district': address['district'],
+                        'ward': address['ward'],
+                        'st_no': address['st_no']}
+        self.priority_group = priority_group
+        self.vaccine_shots = []
+        for shot in vaccine_shots:
+            temp_shot = {'type_name': shot['type_name'],
+                         'shot_num': shot['shot_num'],
+                         'shot_date': parse_to_date(shot['shot_date']),
+                         'shot_place': shot['shot_place'],
+                         'next_shot_expect_time': parse_to_date(shot['next_shot_expect_time'])}
+            self.vaccine_shots.append(temp_shot)
+
+    def gen_dict(self):
+        return {
+            'name': self.name,
+            'birth_day': self.birthday,
+            'sex': self.sex,
+            'phone': self.phone,
+            'email': self.email,
+            'CCCD': self.cccd,
+            'BHXH_id': self.bhxh_id,
+            'address': self.address,
+            'priority_group': self.priority_group,
+            'vaccination_shots': self.vaccine_shots
+        }
+
+
+# Decorated functions
+
+
 def admin_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -90,17 +158,45 @@ def admin_required(f):
 
     return decorated
 
-# TODO: user_require
+
+def login_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        current_user = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return {'result': "fail", 'message': "Token is not found"}
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            current_user = managers.find_one({"_id": ObjectId(data['_id'])})
+
+            print(current_user)
+        except Exception as ignore:
+            print(ignore)
+            return {'result': "fail", 'message': "Token is invalid"}
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
 
 @app.route('/')
 def helloWorld():
     return {'message': 'Đây là API của ứng dụng đăng ký tiêm chủng!'}
 
 
+# PART ONE:
+# Xem danh sách tài khoản, xem 1 tài khoản, tạo tài khoản, nâng cấp tài khoản thành admin và xóa tài khoản
+
+
 @app.route('/user', methods=['GET'])
 @admin_required
 def get_all_users(current_user):
-
     res = {'user': []}
     user = managers.find({})
 
@@ -199,10 +295,49 @@ def login():
                             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
                            app.config['SECRET_KEY'])
 
-        return {'token': token}
+        return {'result': 'success', 'token': token}
 
     return make_response('Could not sign in', 401, {'WWW-authenticate': 'Basic realm="Wrong credential!"'})
 
 
+# NEWS -------------------------------------------------------
+
+@app.route('/news', methods=['GET'])
+def get_news():
+    result = []
+    try:
+        news_arr = news_collection.find({})
+        for news in news_arr:
+            result.append(news)
+
+        return {'result': 'success', 'news': result}
+    except Exception as e:
+        print(e)
+        return {'result': 'fail'}
+
+
+#
+
+
+@app.route('/vaccination-sign', methods=['POST'])
+def insert_sign():
+    data = request.get_json()
+    new_sign = Sign(_id=ObjectId, name=data['name'], birthday=data['birth_day'], sex=data['sex'], phone=data['phone'],
+                    email=data['email'], cccd=data['CCCD'], bhxh_id=data['BHXH_id'], address=data['address'],
+                    priority_group=data['priority_group'], vaccine_shots=data['vaccine_shots'])
+
+    verify = sign_collection.find_one({'CCCD': data['CCCD']})
+    if verify is not None:
+        return {'result': 'fail',
+                'reason': 'You signed in the past'}
+
+    try:
+        sign_collection.insert_one(new_sign.gen_dict())
+        return {'result': 'success'}
+    except Exception as e:
+        print(e)
+        return {'result': 'fail', 'reason': 'not able to add'}
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
