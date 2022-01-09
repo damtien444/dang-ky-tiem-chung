6ac3163ace0c2da237fd952e1782ae67fb9c31b7
anhlam <lamanh28082000@gmail.com>
2022-01-09 21:12:33 +0700

    add search province

--- vaccine_for_the_people/lib/app/modules/injection_statistic/binding/injection_statistic_binding.dart
+++ vaccine_for_the_people/lib/app/modules/injection_statistic/binding/injection_statistic_binding.dart
@@ -1,9 +1,14 @@
 import 'package:get/get.dart';
+import 'package:vaccine_for_the_people/app/modules/injection_statistic/controller/injection_statistic_controller.dart';
+import 'package:vaccine_for_the_people/app/modules/register_injection/data/providers/viet_nam_provider.dart';
+import 'package:vaccine_for_the_people/app/modules/register_injection/data/services/viet_nam_repository.dart';
 
 class InjectionStatistic extends Bindings{
   @override
   void dependencies() {
-    // TODO: implement dependencies
+    Get.lazyPut<InjectionStatisticController>( ()=>
+        InjectionStatisticController(vietNamRepository: VietNamRepository(vnProvider: VnProvider())),
+    );
   }
 
 }
