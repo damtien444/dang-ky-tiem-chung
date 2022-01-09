1c59cf83beae803e1168643ce35269b0cbde342a
damti <damtien444@gmail.com>
2022-01-09 11:45:37 +0700

    phan task todo

--- vaccine/controller/vaccination_campaign.py
+++ vaccine/controller/vaccination_campaign.py
@@ -1,31 +1,39 @@
 from vaccine import app, request
 from vaccine.controller.service import db
 
-
+# Tiến
 # campaign/ POST
 # TODO: create campaign
 
+# Thịnh
 # campaign/ GET
 # TODO: get all campaign
 
+# Thịnh
 # campaign/<campaign-id> GET
 # TODO: get a campaign
 
+# Tiến
 # campaign/<campaign-id> PUT
 # TODO: update or promote a campaign
 
+# Thịnh
 # campaign/<campaign-id> DELETE
 # TODO: delete a campaign
 
+# Huyền
 # campaign/<campaign-id>/user/<user-id> GET
 # TODO: get a person in campaign
 
+# Huyền
 # campaign/<campaign-id>/user/ POST
 # TODO: add a person to campaign
 
+# Huyền
 # campaign/<campaign-id>/user/<user-id> DELETE
 # TODO: delete a person from campaign
 
+# Huyền
 # campaign/<campaign-id>/user/<user-id> PUT
 # TODO: update a person in campaign
 
