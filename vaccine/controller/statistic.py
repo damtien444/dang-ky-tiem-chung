288313bcc8f02cc9fede9223ac9b3bec6bbb971d
damti <damtien444@gmail.com>
2022-01-12 21:08:22 +0700

    change access for test in

--- vaccine/controller/statistic.py
+++ vaccine/controller/statistic.py
@@ -10,8 +10,8 @@ sign = db['vaccination_sign']
 
 
 @app.route('/campaign-statistic', methods=['POST'])
-@admin_required
-def vaccine_statistic_gathering(user):
+# @admin_required
+def vaccine_statistic_gathering():
     try:
 
         data = request.get_json()
